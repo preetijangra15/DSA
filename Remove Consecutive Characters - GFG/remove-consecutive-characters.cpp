43529eba85a11c06d819141b3e1bc18de70e2077
preetijangra15 <68685553+preetijangra15@users.noreply.github.com>
2023-06-14 11:40:30 +0530

    Added solution - LeetHub

---
+++ Remove Consecutive Characters - GFG/remove-consecutive-characters.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string s)
+    {
+        // code here.
+        
+        int n = s.size();
+        string ans = "";
+        
+        for(int i = 0;i<n;i++){
+            
+            if(s[i] == s[i+1])
+                continue;
+                
+            else
+                ans += s[i];
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.removeConsecutiveCharacter(s)<<endl;
+    }
+} 
+
+
+
+
+// } Driver Code Ends
