2d5a84354a59d282241eaa5753d38c545c4a2e81
preetijangra15 <68685553+preetijangra15@users.noreply.github.com>
2023-06-15 09:40:39 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -7,23 +7,73 @@ vector<int> find(int arr[], int n , int x )
 {
     // code here
     
-    int first = INT_MAX, last = INT_MIN;
-    bool flag = false;
-    for(int i = 0; i < n;i++)
-    {
-        if(arr[i] == x)
+    
+    /////Linear search////
+    // int first = INT_MAX, last = INT_MIN;
+    // bool flag = false;
+    // for(int i = 0; i < n;i++)
+    // {
+    //     if(arr[i] == x)
+    //     {
+    //         flag = true;
+    //         first = min(first, i);
+    //         last = max(last, i);
+    //     }
+        
+    // }
+    // if(flag)
+    //     return {first, last};
+        
+    // else
+    //     return {-1, -1};
+    
+    
+    int hi = n-1, lo = 0;
+    int first = -1;
+    while(lo <= hi){
+        
+        int mid = (lo+hi)/2;
+        
+        if(arr[mid] == x)
         {
-            flag = true;
-            first = min(first, i);
-            last = max(last, i);
+            first = mid;
+            hi = mid - 1;
         }
         
-    }
-    if(flag)
-        return {first, last};
+        else if(arr[mid] < x)
+        {
+            lo = mid+1;
+        }
         
-    else
-        return {-1, -1};
+        else
+            hi = mid - 1;
+        
+    }
+    
+    
+    lo = 0, hi = n-1;
+    int last = -1;
+     while(lo <= hi){
+        
+        int mid = (lo+hi)/2;
+        
+        if(arr[mid] == x)
+        {
+            last = mid;
+            lo = mid + 1;
+        }
+        
+        else if(arr[mid] < x)
+        {
+            lo = mid+1;
+        }
+        
+        else
+            hi = mid - 1;
+        
+    }
+    
+    return {first, last};
 }
 
 //{ Driver Code Starts.
