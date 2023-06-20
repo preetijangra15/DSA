3bfddb938d4d676649b38e82dbe7afe7f0138c94
preetijangra15 <68685553+preetijangra15@users.noreply.github.com>
2023-06-20 15:08:18 +0530

    Added solution - LeetHub

---
+++ Minimum Cost of ropes - GFG/minimum-cost-of-ropes.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n) {
+        // Your code here
+        
+        priority_queue<long long int, vector<long long int>, greater<long long int>> pq;
+        
+        for(long long int i=0;i<n;i++)
+            pq.push(arr[i]);
+            
+        long long sum = 0;
+        
+        while(pq.size() != 1){
+            long long x = pq.top();
+            pq.pop();
+            long long y = pq.top();
+            pq.pop();
+            
+            long long s = x + y;
+            
+            pq.push(s);
+            sum += s;
+        }
+        
+        //sum += pq.top();
+        
+        return sum;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long i, a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        cout << ob.minCost(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
