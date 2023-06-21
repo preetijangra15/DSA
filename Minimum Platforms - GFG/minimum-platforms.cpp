d9dbe6e84d77b50e1e2167bfef16153c007beee9
preetijangra15 <68685553+preetijangra15@users.noreply.github.com>
2023-06-21 09:46:24 +0530

    Added solution - LeetHub

---
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -0,0 +1,139 @@
+//{ Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	// Your code here
+    // 	vector<pair<int,int>> v;
+    	
+    // 	for(int i=0;i<n;i++)
+    // 	{
+    // 	    v.push_back(make_pair(arr[i], dep[i]) );
+    // 	}
+    	
+    // 	sort(v.begin(), v.end());
+    	
+    // 	vector<int> ans;
+    // 	ans.push_back(v[0].second);
+    	
+    // 	for(int i=1;i<n;i++)
+    // 	{
+    // 	    bool f = false;
+    	    
+    	   
+    // 	    for(int j = 0;j<ans.size(); j++)
+    // 	    {
+    // 	        if(v[i].first > ans[j])
+    // 	       {
+    // 	           ans[j] = v[i].second;
+    // 	           f = true;
+    // 	           break;
+    // 	       }
+    // 	    }
+    	    
+    // 	    if(f == false)
+    // 	        ans.push_back(v[i].second);
+    	    
+    // 	}
+    	
+    // 	return ans.size();
+    
+    // vector<pair<int, int>> v;
+    
+    // for(int i=0;i<n;i++)
+    // {
+    //     v.push_back(make_pair(arr[i], dep[i]));
+    // }
+    
+    // sort(v.begin(), v.end());
+    
+    // vector<int> ans;
+    // ans.push_back(v[0].second);
+    
+    // for(int i=1;i<n;i++)
+    // {
+    //     bool f = false;
+        
+    //     for(int j = 0; j<ans.size();j++)
+    //     {
+    //         if(v[i].first > ans[j])
+    //         {
+    //             ans[j] = v[i].second;
+    //             f = true;
+    //             break;
+    //         }
+    //     }
+        
+    //     if(f == false)
+    //         ans.push_back(v[i].second);
+    // }
+    
+    // return ans.size();
+    
+    
+    vector<pair<int, int>> v;
+    
+    for(int i = 0;i < n;i++){
+        v.push_back(make_pair(arr[i], dep[i]));
+    }
+    
+    sort(v.begin(), v.end());
+    
+    vector<int> ans;
+    //int count = 0;
+    ans.push_back(v[0].second);
+    for(int i = 1;i < n;i++){
+        
+        bool f = false;
+        
+        for(int j = 0;j < ans.size();j++){
+            if(ans[j] < v[i].first){
+                ans[j] = v[i].second;
+                f=true;
+                break;
+            }
+        }
+        
+        if(!f){
+            ans.push_back(v[i].second);
+        }
+    }
+    
+    return ans.size();
+    	
+    }
+};
+
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}
+// } Driver Code Ends
